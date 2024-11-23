cc374f34e26fa9c1827518b06cb4edd508eb845f
Aibatyr <aibatyr111@gmail.com>
2024-11-23 17:21:26 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/FirstController.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class FirstController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello World!";
+	}
+
+	@PostMapping("/postuser")
+	public ResponseEntity<String> createUser(@RequestBody User user) {
+		userRepository.save(user);
+		String responseMessage = "User " + user.getName() + " saved successfully!";
+		return new ResponseEntity<>(responseMessage, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/getusers")
+	public ResponseEntity<List<User>> getAllUsers() {
+		List<User> users = userRepository.findAll();
+		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
+
+	@GetMapping("/getuser/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable String id) {
+		return userRepository.findById(id)
+				.map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
+
+	@DeleteMapping("/deleteuser/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable String id) {
+		if (userRepository.existsById(id)) {
+			userRepository.deleteById(id);
+			return new ResponseEntity<>("User deleted successfully!", HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("User not found!", HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@PutMapping("/updateuser/{id}")
+	public ResponseEntity<String> updateUser(@PathVariable String id, @RequestBody User updatedUser) {
+		return userRepository.findById(id)
+				.map(user -> {
+					user.setName(updatedUser.getName());
+					userRepository.save(user);
+					return new ResponseEntity<>("User updated successfully!", HttpStatus.OK);
+				})
+				.orElse(new ResponseEntity<>("User not found!", HttpStatus.NOT_FOUND));
+	}
+}
